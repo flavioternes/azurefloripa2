563b256549df1c938ef877ffae792ccca76d4486
flavioternes <52075594+flavioternes@users.noreply.github.com>
2020-08-19 15:35:27 -0300

    Update AzureFloripaPS-Param.ps1

--- AzureFloripaPS-Param.ps1
+++ AzureFloripaPS-Param.ps1
@@ -21,6 +21,8 @@ while(!($connectionResult) -and ($logonAttempt -le 10))
 # Iniciando a VM, utilizando as variáveis com os parâmetros que foram inseridos
 Start-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName
 
+# Aplicando Tags de Localidade
+
 $tags = @{Localidade="Floripa-SC"}
 
 Set-AzResource -ResourceGroupName $ResourceGroupName -Name $VMName -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags
